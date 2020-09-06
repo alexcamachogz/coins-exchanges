bbaa5a2148444aac9ddd89e2c79c7dddd6c05b5c
Alejandra Camacho <camacho.ale94@gmail.com>
2020-09-06 17:57:49 -0500

    Complete information in about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex-col items-center">
+	<div class="flex-col items-center h-screen">
 		<h1 class="text-gray-700 text-6xl">Coins Exchange v1.0.0</h1>
 		<p class="text-gray-600 text-xl text-center">
 			Proyecto para obtener las cotizaciones de las cryptomonedas mas
@@ -7,7 +7,16 @@
 			<br />Este proyecto es utilizado para aprender Vue.js
 		</p>
 
-		<router-link to="/" class="mt-5 text-xl text-pink-600 hover:underline"
+		<div class="text-center mt-12 mb-10">
+			<h3 class="mb-2">🚀 Tecnologías utilizadas</h3>
+			<span class="bg-pink-100 p-3 rounded-lg">
+				· HTML · Tailwind · API Rest · Vue2 ·
+			</span>
+		</div>
+
+		<router-link
+			to="/"
+			class="mt-5 text-base text-pink-600 border-2 border-pink-600 rounded-lg p-3 hover:bg-pink-600 hover:text-white"
 			>Volver a la pagina de Inicio</router-link
 		>
 	</div>
